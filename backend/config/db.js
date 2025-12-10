86b1a574b9609d26604f07b42a99821a78be85b4
Keeyseey <fermilankylaclaire@gmail.com>
2025-12-09 16:16:55 -0800

    Refactor MongoDB connection to use environment variable
    Updated MongoDB connection to use environment variable for URL and added connection options.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
 export const connectDb = async () => {
-    try {
-        await mongoose.connect(
-            'mongodb+srv://fermilankylaclaire:05022005@cluster0.gjqz3iw.mongodb.net/QuickBite'
-        );
-        console.log("MongoDB Atlas connected successfully");
-    } catch (err) {
-        console.error("MongoDB connection error:", err);
-        process.exit(1); // optional: exit app if DB connection fails
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Atlas connected successfully");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
 };
+
